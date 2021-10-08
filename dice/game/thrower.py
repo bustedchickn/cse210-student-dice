7d450db26c52237ac38ea4ba3b56522fcfdaf115
bustedchickn <talmage.kay@gmail.com>
2021-10-08 10:50:36 -0600

    Update thrower.py

--- dice/game/thrower.py
+++ dice/game/thrower.py
@@ -1,3 +1,10 @@
 import random
 
 # TODO: Define the Thrower class here.
+
+class Thrower():
+    def __init__(self) -> None:
+        dice = 5
+        num_throws = 0
+    def throw_dice(self):
+        return([random.randint(1,6),random.randint(1,6),random.randint(1,6),random.randint(1,6),random.randint(1,6),random.randint(1,6)])
